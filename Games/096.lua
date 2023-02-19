1fb5bddef70a31fd09d4b9ecb71c61043bd62dde
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-02-19 18:38:26 +0800

    Update 096.lua

--- Games/096.lua
+++ Games/096.lua
@@ -1,4 +1,3 @@
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("bing chilling | 096", "GrapeTheme")
 
@@ -9,15 +8,15 @@ local TeleportSection = TeleportMain:NewSection("Teleports")
 
 --Buttons
 TeleportSection:NewButton("bunker outside", "ButtonInfo", function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(335.0155334472656, 302.1949462890625, 569.10693359375)
+    	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(335.0155334472656, 302.1949462890625, 569.10693359375)
 end)
 
 TeleportSection:NewButton("fishing dock", "ButtonInfo", function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-485.17022705078125, 150.72828674316406, 1309.5987548828125)
+    	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-485.17022705078125, 150.72828674316406, 1309.5987548828125)
 end)
 
 TeleportSection:NewButton("train area", "ButtonInfo", function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-183.05526733398438, 163.7554931640625, -510.52899169921875)
+    	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-183.05526733398438, 163.7554931640625, -510.52899169921875)
 end)
 
 
@@ -26,14 +25,28 @@ local MiscTab = Window:NewTab("Misc")
 local MiscSection = MiscTab:NewSection("Misc")
 
 MiscSection:NewButton("inf yield", "ButtonInfo", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
 MiscSection:NewButton("fates admin", "ButtonInfo", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
+end)
+
+MiscSection:NewButton("anti hunger and energy", "ButtonInfo", function()
+    	while wait() do
+	local args = {
+    	[1] = "Consume",
+    	[2] = "Lemonade"
+	}		
+
+	game:GetService("ReplicatedStorage").Remotes.Backpack:FireServer(unpack(args))
+	end
+end)
+
+MiscSection:NewButton("Tora GUI", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraIsMe2/ToraIsMe2/main/0scp096", true))()
 end)
 
 MiscSection:NewKeybind("UI Key", "key", Enum.KeyCode.F, function()
 	Library:ToggleUI()
 end)
-
