dd808719701a76cea0f71f755834438726b12115
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-02-19 18:59:28 +0800

    Create main.lua

---
+++ Games/Forgotten-Memories/main.lua
@@ -0,0 +1,22 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("bing chilling | Forgotten Memeories", "GrapeThemee")
+
+--MainTab
+local MainTab = Window:NewTab("Main")
+local MainSection = MainTab:NewSection("Main")
+
+MainSection:NewButton("ReaperHub (Main Stuff Here)", "Main Stuff Here", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/Forgotten-Memories/ReaperHubFree.lua"))()
+end)
+
+MainSection:NewButton("tp", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/Forgotten-Memories/tp.lua"))()
+end)
+
+MainSection:NewButton("block marionette", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/Forgotten-Memories/block-marionette.lua"))()
+end)
+
+MainSection:NewKeybind("UI Key", "key", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
