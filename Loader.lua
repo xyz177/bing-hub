82ab65ceb85c732fd7c5647d8c501b20eedd394b
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-02-19 19:01:00 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,3 +4,6 @@ end
 if game.PlaceId == 12017032683 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/unofficial.lua"))()
 end
+if game.PlaceId == 8482713490 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/Forgotten-Memories/main.lua"))()
+end
