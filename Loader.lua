01cd7b109f87b88d0825cdf75c9b52df77c3326b
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-06-16 21:26:30 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,29 @@
-if game.PlaceId == 8571687919 then
+if game.PlaceId == (8571687919) then
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Bing Hub";
+Text = "Unofficial";
+Duration = 4;
+})
+wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/unofficial.lua"))()
-end 
-if game.PlaceId == 12017032683 then
+
+elseif game.PlaceId == (12017032683) then
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Bing Hub";
+Text = "SCP: Stranded [Horror Survival]";
+Duration = 4;
+})
+wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/096.lua"))()
-end
-if game.PlaceId == 8482713490 then
+
+if game.PlaceId == (8482713490) then
+game.StarterGui:SetCore("SendNotification", {
+Title = "Bing Hub";
+Text = "Forgotten Memories";
+Duration = 4;
+})
+wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/Forgotten-Memories/main.lua"))()
 end
