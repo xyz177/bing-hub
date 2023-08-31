6b3118fa40de788befc65a3a48d1caa097a8bef5
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-08-31 18:52:21 +0800

    Update war-machines.lua

--- Games/war-machines.lua
+++ Games/war-machines.lua
@@ -8,6 +8,27 @@ local Tab2 = window:NewTab("other")
 local Tab2Section = Tab2:NewSection("others")
 
 -- Buttons Tab1
+Tab1Section:NewButton("Disable Hitbox","disable",function()
+_G.HeadSize = 0
+_G.Disabled = true
+
+game:GetService('RunService').RenderStepped:connect(function()
+if _G.Disabled then
+for i,v in next, game:GetService('Players'):GetPlayers() do
+if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+pcall(function()
+v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+v.Character.HumanoidRootPart.Transparency = 0.7
+v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+v.Character.HumanoidRootPart.Material = "Neon"
+v.Character.HumanoidRootPart.CanCollide = false
+end)
+end
+end
+end
+end)
+end)
+
 Tab1Section:NewButton("Hitbox","Increase kill range",function()
 _G.HeadSize = 25
 _G.Disabled = true
