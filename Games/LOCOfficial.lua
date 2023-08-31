efec3b7717d0ecb84be8545b52bb844837ad397d
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-08-31 18:36:48 +0800

    Update and rename unofficial.lua to LOCOfficial.lua

--- Games/LOCOfficial.lua
+++ Games/LOCOfficial.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("bing chilling | UNOFFICIAL", "GrapeTheme")
+local Window = Library.CreateLib("bing chilling | LOCOfficial", "GrapeTheme")
 
 --Main
 
