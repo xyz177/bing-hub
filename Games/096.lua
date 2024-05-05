11c1624fea864992fa896d376e9496e097fe4591
xyz177 <112547284+xyz177@users.noreply.github.com>
2024-05-05 19:14:51 +0800

    Update 096.lua

--- Games/096.lua
+++ Games/096.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("bing chilling | 096", "GrapeTheme")
+local Window = Library.CreateLib("096 | Bing Hub", "GrapeTheme")
 
 --TeleportTab
 local TeleportMain = Window:NewTab("Teleports")
