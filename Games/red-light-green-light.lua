6dfcaa6e8eab475835702523c8ae1fdfd69cf17a
xyz177 <112547284+xyz177@users.noreply.github.com>
2024-05-05 19:17:18 +0800

    Update red-light-green-light.lua

--- Games/red-light-green-light.lua
+++ Games/red-light-green-light.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("bing chilling | Red Light, Green Light", "GrapeTheme")
+local Window = Library.CreateLib("Red Light, Green Light | Bing Hub", "GrapeTheme")
 
 --lobby
 
