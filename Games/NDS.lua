defd003789840df0c12efa2ca1b44f00029f6686
xyz177 <112547284+xyz177@users.noreply.github.com>
2024-05-10 20:03:11 +0800

    Update NDS.lua

--- Games/NDS.lua
+++ Games/NDS.lua
@@ -4,15 +4,15 @@ local Window = Library.CreateLib("NDS | Bing Hub", "GrapeTheme")
 local MainTab = Window:NewTab("Main")
 local MainSection = MainTab:NewSection("Main")
 
-MainSection:NewButton("Garfield Ui", function()
+MainSection:NewButton("Garfield Ui", "yes", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/NDS/nds.lua"))()
 end)
 
-MainSection:NewButton("check green balloon",function()
+MainSection:NewButton("check green balloon","yes" ,function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/NDS/ballooncheck.lua"))()
 end)
 
-MainSection:NewButton("Inf Yield", function()
+MainSection:NewButton("Inf Yield", "yes", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
