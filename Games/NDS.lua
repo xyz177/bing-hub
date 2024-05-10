bebf1e9593f45b379e71a1af703df741de3c44b2
xyz177 <112547284+xyz177@users.noreply.github.com>
2024-05-10 20:00:28 +0800

    Create NDS.lua

---
+++ Games/NDS.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("NDS | Bing Hub", "GrapeTheme")
+
+local MainTab = Window:NewTab("Main")
+local MainSection = MainTab:NewSection("Main")
+
+MainSection:NewButton("Garfield Ui", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/NDS/nds.lua"))()
+end)
+
+MainSection:NewButton("check green balloon",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/bing-hub/main/Games/NDS/ballooncheck.lua"))()
+end)
+
+MainSection:NewButton("Inf Yield", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+MainSection:NewKeybind("UI Key", "KeybindInfo", Enum.KeyCode.X, function()
+	Library:ToggleUI()
+end)
