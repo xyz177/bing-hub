74a9af1173fd3929ac06871fe6b2b2f0ffa457aa
xyz177 <112547284+xyz177@users.noreply.github.com>
2025-12-17 12:27:48 +0800

    changed ui lib 096.lua

--- Games/096.lua
+++ Games/096.lua
@@ -1,46 +1,43 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("096 | Bing Hub", "GrapeTheme")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/archive/refs/heads/main/ui-libs/mobilefriendlyui.lua"))()
+local window = Library.Window("096 | Bing Hub")
 
 --TeleportTab
-local TeleportMain = Window:NewTab("Teleports")
-local TeleportSection = TeleportMain:NewSection("Teleports")
-
+local TeleportTab = Window:NewTab("Teleports")
 
 --Buttons
-TeleportSection:NewButton("cave", "ButtonInfo", function()
+TeleportTab:Button("cave", function()
     	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(934.6703, -188.1975, 561.8464, -0.9986, 0, -0.0532, 0, 1, 0, 0.0532, 0, -0.9986)
 end)
 
-TeleportSection:NewButton("bunker outside", "ButtonInfo", function()
+TeleportTab:Button("bunker outside", function()
     	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(335.0155334472656, 302.1949462890625, 569.10693359375)
 end)
 
-TeleportSection:NewButton("bunker inside", "ButtonInfo", function()
+TeleportTab:Button("bunker inside", function()
     	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1155.1910400390625, -63.2086067199707, -581.080810546875)
 end)
 
-TeleportSection:NewButton("fishing dock", "ButtonInfo", function()
+TeleportTab:Button("fishing dock", function()
     	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-485.17022705078125, 150.72828674316406, 1309.5987548828125)
 end)
 
-TeleportSection:NewButton("train area", "ButtonInfo", function()
+TeleportTab:Button("train area", function()
     	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-183.05526733398438, 163.7554931640625, -510.52899169921875)
 end)
 
 
 --Misc
 local MiscTab = Window:NewTab("Misc")
-local MiscSection = MiscTab:NewSection("Misc")
 
-MiscSection:NewButton("inf yield", "ButtonInfo", function()
+MiscTab:Button("inf yield", function()
     	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
-MiscSection:NewButton("fates admin", "ButtonInfo", function()
+MiscTab:Button("fates admin", function()
     	loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
 end)
 
-MiscSection:NewButton("anti hunger and energy", "ButtonInfo", function()
+MiscTab:Button("anti hunger and energy", function()
     	while wait() do
 	local args = {
     	[1] = "Consume",
@@ -51,10 +48,10 @@ MiscSection:NewButton("anti hunger and energy", "ButtonInfo", function()
 	end
 end)
 
-MiscSection:NewButton("Tora GUI", "ButtonInfo", function()
+MiscTab:Button("Tora GUI", "ButtonInfo", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraIsMe2/ToraIsMe2/main/0scp096", true))()
 end)
 
-MiscSection:NewKeybind("UI Key", "key", Enum.KeyCode.F, function()
+MiscTab:Keybind("UI Key", "key", Enum.KeyCode.RightShift, function()
 	Library:ToggleUI()
 end)
